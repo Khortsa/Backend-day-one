9723ebc651c49cf19cd72fba61264ed0e600c3f9
Tenzin Choezom <khortsa2021@gmail.com>
2022-04-11 16:47:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+
+const express = require('express');
+const port = 3000;
+
+const app = express();
+
+var authors = [
+    {
+        name: "Lawrence Nowell",
+        nationality: "UK",
+        books: ["Beowulf"]
+    },
+    {
+        name: "William Shakespeare",
+        nationality: "UK",
+        books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"]
+    },
+    {
+        name: "Charles Dickens",
+        nationality: "US",
+        books: ["Oliver Twist", "A Christmas Carol"]
+    },
+    {
+        name: "Oscar Wilde",
+        nationality: "UK",
+        books: ["The Picture of Dorian Gray", "The Importance of Being Earnest"]
+    },
+]
+
+
+app.get('/', (req, res) => {
+    console.log(req);
+    res.send('Authors API');
+    
+});
+
+app.get('/authors/:id/', (req, res) => {
+    let authorName = req.params.id;
+    res.send(`${authors[authorName].name}, ${authors[authorName].nationality}`)
+});
+app.get('/authors/:id/books', (req, res) => {
+    let authorName = req.params.id;
+    res.send(`${authors[authorName].books}`)
+});
+
+app.get('/json/authors/:id/', (req, res) =>{
+    let authorName = req.params.id;
+    res.json({
+        name: `${authors[authorName].name}`,
+        nationality: `${authors[authorName].nationality}`
+    });
+})
+
+
+app.listen(port, () =>{
+   console.log(`server on port: ${port}`);
+});
